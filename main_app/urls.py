5d25c918294f5794f22bbaca81c3b27076575faa
gwalker24 <georginawalker@live.co.uk>
2025-06-01 11:13:26 +0100

    updates with edit/delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, AddToDoView
+from .views import HomeView, AddToDoView, complete_task, delete_task, EditToDoView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('add/', AddToDoView.as_view(), name='add'),
-
-   
+    path('complete-task/<int:task_id>/', complete_task, name='complete_task'),
+    path('delete-task/<int:task_id>/', delete_task, name='delete_task'),
+    path('edit-task/<int:task_id>/', EditToDoView.as_view(), name='edit_task'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
 from .models import Category, Priority, Task
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 
 class HomeView(TemplateView):
@@ -42,3 +45,45 @@ class AddToDoView(View):
         )
 
         return redirect('home')  # ✅ Redirect to home after task is added
+
+
+def complete_task(request, task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=task_id)
+        task.completed = not task.completed  # Toggle completed status
+        task.save()
+    return HttpResponseRedirect(reverse('home'))
+
+
+def delete_task(request, task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=task_id)
+        task.delete()
+    return HttpResponseRedirect(reverse('home'))
+
+
+class EditToDoView(View):
+    template_name = 'add.html'
+
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        categories = Category.objects.all()
+        priorities = Priority.objects.all()
+        return render(request, self.template_name, {
+            'task': task,
+            'categories': categories,
+            'priorities': priorities,
+        })
+
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        category_id = request.POST.get('category')
+        priority_id = request.POST.get('priority')
+        task.category = Category.objects.filter(id=category_id).first()
+        task.priority = Priority.objects.filter(id=priority_id).first()
+        task.save()
+        return redirect('home')
+
+
