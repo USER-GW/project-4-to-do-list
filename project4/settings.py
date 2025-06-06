df2447b4878603c0423a0ae16051f4d1d411b365
gwalker24 <georginawalker@live.co.uk>
2025-06-06 21:37:03 +0100

    updates

--- project4/settings.py
+++ project4/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['project-4-to-do-list.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'project-4-to-do-list.onrender.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
